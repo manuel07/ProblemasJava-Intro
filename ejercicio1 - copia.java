f2b7d514756dab0702118b70f5f2697c2d0424dd
cgonzales97 <cristhiangonzales@unitec.edu>
2015-03-11 07:28:08 -0600

    agregar archivo

---
+++ ejercicio1 - copia.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ejercicio1{
+	public static void main(String[] args){
+		Scanner lea = new Scanner(System.in);
+		String nombre
+		int edad, cant, tipo, total;
+
+		System.out.print("")
